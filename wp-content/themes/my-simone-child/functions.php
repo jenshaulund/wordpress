ad6d484f61bb2eb786df5b7a019deadb39684c2b
Jens Haulund <jenshaulund@yahoo.com>
2016-01-18 20:18:54 -0500

    Monday night before Emma pick up

--- wp-content/themes/my-simone-child/functions.php
+++ wp-content/themes/my-simone-child/functions.php
@@ -5,6 +5,17 @@
  * @package my-simone
  */
 
+function rf_add_reviews( $query ){
+	if( !is_admin() && $query->is_main_query() ){
+		if( $query->is_home()){
+			$query->set( 'post_type', array( 'post', 'reviews'));
+		}
+	}
+
+}
+
+add_action( 'pre_get_posts', 'rf_add_reviews' );
+
 function custom_excerpt_length( $length ) {
 	return 20;
 }
