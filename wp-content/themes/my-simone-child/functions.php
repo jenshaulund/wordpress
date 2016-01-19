30b14850f9313241e02869cac00db9f9c9bd1f06
Jens Haulund <jenshaulund@yahoo.com>
2016-01-19 18:38:28 -0500

    Resolved conflicet

--- wp-content/themes/my-simone-child/functions.php
+++ wp-content/themes/my-simone-child/functions.php
@@ -15,11 +15,7 @@ function rf_add_reviews( $query ){
 add_action( 'pre_get_posts', 'rf_add_reviews' );
 
 function custom_excerpt_length( $length ) {
-<<<<<<< HEAD
 	return 60;
-=======
-	return 30;
->>>>>>> ee631770fbe37794cbcc6e1d85eb5737c6ba3383
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
